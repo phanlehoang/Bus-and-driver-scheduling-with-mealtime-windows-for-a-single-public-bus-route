9ce3544b9fb7bf78a84a94aa5c4ca48708552fa2
Phan Lê Hoàng <90677680+phanlehoang@users.noreply.github.com>
2023-04-25 14:28:15 +0700

    update

--- bus_driver_scheduling.py
+++ bus_driver_scheduling.py
@@ -13,37 +13,40 @@ class BusDriverScheduling:
                                            ), vtype=GRB.BINARY,
                                     name="x")
         self.roster = np.zeros(len(self.driver_tasks))
+    def virtual_task_constr(self):
+        for d in range(len(self.drivers)):
+            self.model.addConstr(self.x[d,0,0] == 0, name = f"virtual_task_0_{d}")
+            self.model.addConstr(self.x[d,len(self.tasks)+1, len(self.tasks)+1 ] == 0, name = f"virtual_task_(n+1)_{d}")
+            for i in range(1, len(self.tasks)+1):
+                self.model.addConstr(self.x[d,i,i] == 0, name = f"virtual_task_{i}_{d}")
     def select_first_task_constr(self):
         for i in range(len(self.drivers)):
-            self.model.addConstr(sum(self.x[i,0,:]) <= 1,name = f"first_task_{i}")    
+            self.model.addConstr(sum(self.x[i,0, 1: len(self.tasks)+1]) <= 1,name = f"first_task_{i}")    
     def select_last_task_constr(self):
         for i in range(len(self.drivers)):
-            self.model.addConstr(sum(self.x[i,:,
+            self.model.addConstr(sum(self.x[i,1:len(self.tasks)+1,
                                         len(self.tasks)+1]) <= 1,name = f"last_task_{i}")
     def flow_balance_constr(self):
         for d in range(len(self.drivers)):
-            for i in range(0, len(self.tasks)+2):
+            for i in range(1, len(self.tasks)+1):
                 self.model.addConstr(sum(self.x[d,i,:]) - sum(self.x[d,:,i]) == 0, name = f"flow_balance_{d}_{i}")
     def select_one_task_constr(self):
         for task in range(1, len(self.tasks)+1):
             list_variable = [self.x[d,taski,task] for d in range(len(self.drivers))
                           for taski in range(0, len(self.tasks)+1)]
-            self.model.addConstr(sum(list_variable) == 1, name = f"select_one_task_{task} ")
+            self.model.addConstr(sum(list_variable) == 1, name = f"select_one_task_{task}")
 
     def consecutive_task_constr(self):
         for d in range(len(self.drivers)):
-            for i in range(0, len(self.tasks)+1):
-                for j in range(0, len(self.tasks)+1):
-                    if(i>=j):
-                        self.model.addConstr(self.x[d,i,j ]==0)
-                    
+            for i in range(1, len(self.tasks)+1):
+                for j in range(1, len(self.tasks)+1):
                     if(self.tasks[i-1].time_end > self.tasks[j-1].time_start):
-                        self.model.addConstr(self.x[d,i,j ]==0, name = f"consecutive_task_{d}_{i}_{j}")
+                        self.model.addConstr(self.x[d,i,j]==0, name = f"consecutive_task_{d}_{i}_{j}")
     def limit_tasks_each_driver(self):
         for d in range(len(self.drivers)):
             list_variable = [self.x[d,taski,taskj]
                                  for taski in range(1, len(self.tasks)+1)
-                                 for taskj in range(1, len(self.tasks)+1)]
+                                 for taskj in range(1, len(self.tasks)+2)]
             num_tasks = sum(list_variable)
             self.model.addConstr(num_tasks >= self.drivers[d].min_tasks, name = f"min_tasks_{d}")
             self.model.addConstr(num_tasks <= self.drivers[d].max_tasks, name = f"max_tasks_{d}")
